272ab57966d54821b4f73d01a262bae86b9efa49
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-27 19:52:09 +0530

    DSA 1 Selection sort

--- Sorting.java
+++ Sorting.java
@@ -1,4 +1,4 @@
-class BubbleSort {
+class Sorting {
     public static void printArray(int arr[]) {
         for(int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -9,17 +9,22 @@ class BubbleSort {
     public static void main(String[] args) {
         int arr[] = {7, 8, 3, 1, 2};
 
-        // Bubble Sort
+        // selection Sort
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+            int smallest =i;
+            for (int j = i +1; j < arr.length - 1; j++) {
+                if (arr[smallest] > arr[j]) {
+                smallest = j;
+                }
+            }
+                {
                     // Swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    int temp = arr[smallest];
+                    arr[smallest] = arr[i];
+                    arr[i] = temp;
                 }
             }
             printArray(arr); // Print the array after each outer loop iteration
         }
     }
-}
+
