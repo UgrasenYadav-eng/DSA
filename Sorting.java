e0f43356f5f4d4edc5546de3855f3c45093b683e
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-07-27 20:15:56 +0530

    DSA Day 1 Insertion sort

--- Sorting.java
+++ Sorting.java
@@ -9,22 +9,20 @@ class Sorting {
     public static void main(String[] args) {
         int arr[] = {7, 8, 3, 1, 2};
 
-        // selection Sort
-        for (int i = 0; i < arr.length - 1; i++) {
-            int smallest =i;
-            for (int j = i +1; j < arr.length - 1; j++) {
-                if (arr[smallest] > arr[j]) {
-                smallest = j;
-                }
-            }
-                {
-                    // Swap
-                    int temp = arr[smallest];
-                    arr[smallest] = arr[i];
-                    arr[i] = temp;
-                }
-            }
-            printArray(arr); // Print the array after each outer loop iteration
+        // insertion  Sort
+        for(int i = 1; i < arr.length; i++){
+
+            int current= arr[i];
+            int j = i -1;
+            while (j >= 0 && current <arr[j]){
+            arr[j + 1] = arr[j];
+            j--;
         }
+        //placement
+        arr[j + 1] = current;
+    
     }
+    printArray(arr);
+}
+}
 
